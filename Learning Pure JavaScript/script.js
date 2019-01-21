8f1f3bc79633b48fd42d4c7554b46b745a23384e
SushilBajracharya <bajracharya.sushil01@gmail.com>
2019-01-21 22:38:00 +0545

    Learning Js

---
+++ Learning Pure JavaScript/script.js
@@ -0,0 +1,4 @@
+console.log("Learning JS\n``making Web pages  alive");
+
+console.log("JavaScript engines: Chrome: V8\nFireFox: SpiderMonkey\nMicrosoft Edge: ChakraCore.\nThe engine reads the script\nconverts complies the script in machine language.\nthe machine code runs.\nOptimization is applied in each step of the process.");
+
